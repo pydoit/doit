c331684e424936bcfbb68efa4dca36d6725609c7
Eduardo Schettino <schettino72@gmail.com>
2008-04-12 16:53:58 +0530

    include revision.txt in source distribtion.
    --HG--
    extra : convert_revision : schettino72%40gmail.com-20080412112358-g4uu2m5m4vengakh

--- release.py
+++ release.py
@@ -7,7 +7,8 @@ def task_revision():
     return "bzr version-info > revision.txt"
 
 def task_manifest():
-    return "bzr ls --versioned > MANIFEST"
+    return "bzr ls --versioned > MANIFEST;echo 'revision.txt' >> MANIFEST"
+
 
 def task_sdist():
     return "python setup.py sdist"
