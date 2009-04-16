148ab12f3a904edc50de5321457aaeda6135b521
Eduardo Schettino <schettino72@gmail.com>
2009-04-17 00:05:00 +0800

    update setup.py for 0.2 release
    --HG--
    extra : convert_revision : schettino72%40gmail.com-20090416160500-bw38ia2elbqons29

--- setup.py
+++ setup.py
@@ -1,30 +1,37 @@
+
 from distutils.core import setup
 
 setup(name = 'doit',
       description = 'doit - Automation Tool',
-      version = '0.1.0',
+      version = '0.2.0',
       license= 'MIT',
       author = 'Eduardo Naufel Schettino',
       author_email = 'schettino72@gmail.com',
       url = 'http://python-doit.sourceforge.net/',
-      classifiers = ['Development Status :: 3 - Alpha',
+      classifiers = ['Development Status :: 4 - Beta',
                      'Environment :: Console',
                      'Intended Audience :: Developers',
+                     'Intended Audience :: System Administrators',
                      'License :: OSI Approved :: MIT License',
                      'Natural Language :: English',
+                     'Operating System :: OS Independent',
                      'Operating System :: POSIX',
+                     'Programming Language :: Python :: 2.4',
+                     'Programming Language :: Python :: 2.5',
+                     'Programming Language :: Python :: 2.6',
                      'Topic :: Software Development :: Build Tools',
+                     'Topic :: Software Development :: Testing',
                      'Topic :: Software Development :: Quality Assurance',
-                     FIXME
                      ],
 
       packages = ['doit'],
       package_dir = {'':'lib'},
       scripts = ['bin/doit'],
       
-      long_description = FIXME"""
-        doit is a build tool that focus not only on making/building things but 
-        on executing any kind of tasks in an efficient way. Designed to be easy
-        to use and "get out of your way"."""
+      long_description = """
+        doit comes from the idea of bringing the power of build-tools to 
+        execute any kind of task. It will keep track of dependencies between 
+        "tasks" and execute them only when necessary. It was designed to be 
+        easy to use and "get out of your way"."""
       )
 
