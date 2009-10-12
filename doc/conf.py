b1278ce1c99d97f6e1cc90496f6704217cca7b1b
Eduardo Schettino <schettino72@gmail.com>
2009-10-12 23:10:55 +0800

    trivial sphinx changes.
    --HG--
    extra : convert_revision : schettino72%40gmail.com-20091012151055-dnqfcr2vbrfvrzdw

--- doc/conf.py
+++ doc/conf.py
@@ -105,7 +105,7 @@ html_theme = 'sphinxdoc'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "doit - automation tool"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -143,7 +143,7 @@ html_sidebars = {'index':'homesidebar.html'}
 #html_use_modindex = True
 
 # If false, no index is generated.
-#html_use_index = True
+html_use_index = False
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
