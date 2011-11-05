5d8cd7574497851d27f6ad3d0b1417a10f6baff9
Eduardo Schettino <schettino72@gmail.com>
2011-11-05 23:25:07 +0800

    docs. improve quick start example. add doit-recibes on side-bar.
    --HG--
    extra : convert_revision : schettino72-20111105152507-m1a53om4dhuqutag

--- doc/tutorial/hello.py
+++ doc/tutorial/hello.py
@@ -2,12 +2,11 @@ def task_hello():
     """hello from shell & python! """
 
     def python_hello(targets):
-        output = open(targets[0], "a")
-        output.write("Python says Hello World!!!\n")
-        output.close()
+        with open(targets[0], "a") as output:
+            output.write("Python says Hello World!!!\n")
 
     return {'actions': ['echo Hello World!!! > %(targets)s',
-                        (python_hello,)],
+                        python_hello],
             'targets': ["hello.txt"]
             }
 
