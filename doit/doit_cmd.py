2dff6d7de9e3fd94ee6d05bd5cf3b2c80c8ce4f7
Eduardo Schettino <schettino72@gmail.com>
2012-09-25 02:19:41 +0800

    create DoitCmdBase.

--- doit/doit_cmd.py
+++ doit/doit_cmd.py
@@ -7,7 +7,7 @@ import traceback
 import doit
 from .exceptions import InvalidDodoFile, InvalidCommand, InvalidTask
 from . import loader
-from .cmdparse import CmdParseError
+from .cmdparse import CmdParseError, CmdOption
 from .cmd_base import Command
 from .cmd_run import doit_run
 from .cmd_clean import doit_clean
@@ -18,28 +18,89 @@ from .cmd_auto import doit_auto
 
 
 
+HELP_TASK = """
+
+Task Dictionary parameters
+--------------------------
+
+Tasks are defined by functions starting with the string ``task_``. It must return a dictionary describing the task with the following fields:
+
+actions [required]:
+  - type: Python-Task -> tuple (callable, `*args`, `**kwargs`)
+  - type: Cmd-Task -> string or list of strings (each item is a different command). to be executed by shell.
+  - type: Group-Task -> None.
+
+basename:
+  - type: string. if present use it as task name instead of taking name from python function
+
+name [required for sub-task]:
+  - type: string. sub-task identifier
+
+file_dep:
+  - type: list. items:
+    * file (string) path relative to the dodo file
+
+task_dep:
+  - type: list. items:
+    * task name (string)
+
+targets:
+  - type: list of strings
+  - each item is file-path relative to the dodo file (accepts both files and folders)
+
+uptodate:
+  - type: list. items:
+    * None - None values are just ignored
+    * bool - False indicates task is not up-to-date
+    * callable - returns bool or None. must take 2 positional parameters (task, values)
+
+calc_dep:
+  - type: list. items:
+    * task name (string)
+
+getargs:
+  - type: dictionary
+    * key: string with the name of the function parater (used in a python-action)
+    * value: string on the format <task-name>.<variable-name>
+
+setup:
+ - type: list. items:
+   * task name (string)
+
+teardown:
+ - type: (list) of actions (see above)
+
+doc:
+ - type: string -> the description text
+
+clean:
+ - type: (True bool) remove target files
+ - type: (list) of actions (see above)
+
+params:
+ - type: (list) of dictionaries containing:
+   - name [required] (string) parameter identifier
+   - default [required] default value for parameter
+   - short [optional] (string - 1 letter) short option string
+   - long [optional] (string) long option string
+   - type [optional] the option will be converted to this type
+
+verbosity:
+ - type: int
+   -  0: capture (do not print) stdout/stderr from task.
+   -  1: (default) capture stdout only.
+   -  2: do not capture anything (print everything immediately).
+
+title:
+ - type: callable taking one parameter as argument (the task reference)
+"""
+
 
 ## cmd line options
 ##########################################################
-########## run
-# print version and exit
-opt_version = {'name': 'version',
-               'short':'',
-               'long': 'version',
-               'type': bool,
-               'default': False,
-               'help': "show version"
-               }
 
-# display cmd line usage
-opt_help = {'name': 'help',
-            'short':'',
-            'long': 'help',
-            'type': bool,
-            'default': False,
-            'help':"show help"
-            }
 
+#### options related to dodo.py
 # select dodo file containing tasks
 opt_dodo = {'name': 'dodoFile',
             'short':'f',
@@ -68,6 +129,7 @@ opt_seek_file = {'name': 'seek_file',
                  'help': ("seek dodo file on parent folders " +
                           "[default: %(default)s]")
                  }
+######################################################
 
 # select output file
 opt_outfile = {'name': 'outfile',
@@ -148,78 +210,47 @@ opt_reporter = {'name':'reporter',
                  }
 
 
-def _path_params(params):
-    return (params['dodoFile'], params['cwdPath'], params['seek_file'],
+
+
+class DoitCmdBase(Command):
+    base_options = (opt_dodo, opt_cwd, opt_seek_file, opt_depfile)
+
+    def set_options(self):
+        opt_list = self.base_options + self.cmd_options
+        return [CmdOption(opt) for opt in opt_list]
+
+    def read_dodo(self, params, args):
+        # FIXME should get a tuple instead of dict
+        dodo_tasks = loader.get_tasks(
+            params['dodoFile'], params['cwdPath'], params['seek_file'],
             params['sub'].keys())
+        self.task_list = dodo_tasks['task_list']
+        self.config = dodo_tasks['config']
+        params.update_defaults(self.config)
+        self.sel_tasks = args or self.config.get('default_tasks')
+        return params
 
 
-def print_version():
-    """print doit version (includes path location)"""
-    print ".".join([str(i) for i in doit.__version__])
-    print "bin @", os.path.abspath(__file__)
-    print "lib @", os.path.dirname(os.path.abspath(doit.__file__))
-
-
-def print_usage():
-    """print doit "usage" (basic help) instructions"""
-    # TODO cmd list should be automatically generated.
-    print """
-doit -- automation tool
-http://python-doit.sourceforge.net/
-
-Commands:
- doit [run]             run tasks
- doit clean             clean action / remove targets
- doit list              list tasks from dodo file
- doit forget            clear successful run status from DB
- doit ignore            ignore task (skip) on subsequent runs
- doit auto              automatically run doit when a dependency changes
-
- doit help              show help / reference
- doit help task         show help on task dictionary fields
- doit help <command>    show command usage
-"""
-
-class Run(Command):
+class Run(DoitCmdBase):
     doc_purpose = "run tasks"
     doc_usage = "[TASK/TARGET...]"
     doc_description = None
 
-    cmd_options = (opt_version, opt_help, opt_dodo, opt_cwd, opt_depfile,
-                   opt_seek_file, opt_always, opt_continue, opt_verbosity,
+    cmd_options = (opt_always, opt_continue, opt_verbosity,
                    opt_reporter, opt_outfile, opt_num_process)
-
 
     _execute = staticmethod(doit_run)
 
+
     def execute(self, params, args):
         """execute cmd 'run' """
-
-        # FIXME move this out of run command
-        # special parameters that dont run anything
-        if params["version"]:
-            print_version()
-            return 0
-        if params["help"]:
-            print_usage()
-            return 0
-
-        # check if no sub-command specified. default command is "run"
-        if len(args) == 0 or args[0] not in params['sub']:
-            dodo_tasks = loader.get_tasks(*_path_params(params))
-            params.update_defaults(dodo_tasks['config'])
-            default_tasks = args or dodo_tasks['config'].get('default_tasks')
-            return self._execute(
-                params['dep_file'], dodo_tasks['task_list'],
-                params['outfile'], default_tasks,
-                params['verbosity'], params['always'],
-                params['continue'], params['reporter'],
-                params['num_process'])
-
-        # explicit sub-cmd. parse arguments again
-        commands = params['sub']
-        sub_cmd = args.pop(0)
-        return commands[sub_cmd].parse_execute(args, **params)
+        params = self.read_dodo(params, args)
+        return self._execute(
+            params['dep_file'], self.task_list,
+            params['outfile'], self.sel_tasks,
+            params['verbosity'], params['always'],
+            params['continue'], params['reporter'],
+            params['num_process'])
 
 
 
@@ -265,23 +296,21 @@ opt_list_dependencies = {'name': 'list_deps',
                                   "(file dependencies only)")
                          }
 
-class List(Command):
+class List(DoitCmdBase):
     doc_purpose = "list tasks from dodo file"
     doc_usage = "[TASK ...]"
     doc_description = None
 
-    cmd_options = (opt_dodo, opt_depfile, opt_cwd, opt_seek_file , opt_listall,
-                    opt_list_quiet, opt_list_status, opt_list_private,
-                    opt_list_dependencies)
+    cmd_options = (opt_listall, opt_list_quiet, opt_list_status,
+                   opt_list_private, opt_list_dependencies)
 
     _execute = staticmethod(doit_list)
 
     def execute(self, params, args):
         """execute cmd 'list' """
-        dodo_tasks = loader.get_tasks(*_path_params(params))
-        params.update_defaults(dodo_tasks['config'])
+        params = self.read_dodo(params, args)
         return self._execute(
-            params['dep_file'], dodo_tasks['task_list'], sys.stdout,
+            params['dep_file'], self.task_list, sys.stdout,
             args, params['all'], not params['quiet'],
             params['status'], params['private'], params['list_deps'])
 
@@ -313,25 +342,23 @@ opt_clean_cleanall = {
     'help': 'clean all task'}
 
 
-class Clean(Command):
+class Clean(DoitCmdBase):
     doc_purpose = "clean action / remove targets"
     doc_usage = "[TASK ...]"
     doc_description = ("If no task is specified clean default tasks and "
                        "set --clean-dep automatically.")
 
-    cmd_options = (opt_dodo, opt_cwd, opt_seek_file, opt_clean_cleandep,
-                   opt_clean_cleanall, opt_clean_dryrun)
+    cmd_options = (opt_clean_cleandep, opt_clean_cleanall, opt_clean_dryrun)
 
     _execute = staticmethod(doit_clean)
 
     def execute(self, params, args):
         """execute cmd 'clean' """
-        dodo_tasks = loader.get_tasks(*_path_params(params))
-        params.update_defaults(dodo_tasks['config'])
+        params = self.read_dodo(params, args)
+        default_tasks = self.config.get('default_tasks')
         selected_tasks = args
-        default_tasks = dodo_tasks['config'].get('default_tasks')
         return self._execute(
-            dodo_tasks['task_list'], sys.stdout, params['dryrun'],
+            self.task_list, sys.stdout, params['dryrun'],
             params['cleandep'], params['cleanall'],
             default_tasks, selected_tasks)
 
@@ -340,66 +367,59 @@ class Clean(Command):
 ##########################################################
 ########## forget
 
-class Forget(Command):
+class Forget(DoitCmdBase):
     doc_purpose = "clear successful run status from internal DB"
     doc_usage = "[TASK ...]"
     doc_description = None
 
-    cmd_options = (opt_dodo, opt_cwd, opt_seek_file, opt_depfile,)
-
+    cmd_options = ()
     _execute = staticmethod(doit_forget)
 
     def execute(self, params, args):
         """execute cmd 'forget' """
-        dodo_tasks = loader.get_tasks(*_path_params(params))
-        params.update_defaults(dodo_tasks['config'])
-        options = args or dodo_tasks['config'].get('default_tasks')
+        params = self.read_dodo(params, args)
         return self._execute(
-            params['dep_file'], dodo_tasks['task_list'],
-            sys.stdout, options)
+            params['dep_file'], self.task_list,
+            sys.stdout, self.sel_tasks)
 
 
 ##########################################################
 ########## ignore
 
-class Ignore(Command):
+class Ignore(DoitCmdBase):
     doc_purpose = "ignore task (skip) on subsequent runs"
     doc_usage = "TASK [TASK ...]"
     doc_description = None
 
-    cmd_options = (opt_dodo, opt_cwd, opt_seek_file, opt_depfile,)
+    cmd_options = ()
 
     _execute = staticmethod(doit_ignore)
 
     def execute(self, params, args):
         """execute cmd 'ignore' """
-        dodo_tasks = loader.get_tasks(*_path_params(params))
-        params.update_defaults(dodo_tasks['config'])
+        params = self.read_dodo(params, args)
         return self._execute(
-            params['dep_file'], dodo_tasks['task_list'],
+            params['dep_file'], self.task_list,
             sys.stdout, args)
 
 
 ##########################################################
 ########## auto
 
-class Auto(Command):
+class Auto(DoitCmdBase):
     doc_purpose = "automatically execute tasks when a dependency changes"
     doc_usage = "TASK [TASK ...]"
     doc_description = None
 
-    cmd_options = (opt_dodo, opt_cwd, opt_seek_file, opt_depfile,
-                   opt_verbosity)
+    cmd_options = (opt_verbosity,)
 
     _execute = staticmethod(doit_auto)
 
     def execute(self, params, args):
         """execute cmd 'auto' """
-        dodo_tasks = loader.get_tasks(*_path_params(params))
-        params.update_defaults(dodo_tasks['config'])
-        filter_tasks = args or dodo_tasks['config'].get('default_tasks')
+        params = self.read_dodo(params, args)
         return self._execute(
-            params['dep_file'], dodo_tasks['task_list'], filter_tasks,
+            params['dep_file'], self.task_list, self.sel_tasks,
             params['verbosity'])
 
 
@@ -407,85 +427,6 @@ class Auto(Command):
 ########## help
 
 
-
-def print_task_help():
-    """print help for 'task' usage """
-    print """
-
-Task Dictionary parameters
---------------------------
-
-Tasks are defined by functions starting with the string ``task_``. It must return a dictionary describing the task with the following fields:
-
-actions [required]:
-  - type: Python-Task -> tuple (callable, `*args`, `**kwargs`)
-  - type: Cmd-Task -> string or list of strings (each item is a different command). to be executed by shell.
-  - type: Group-Task -> None.
-
-basename:
-  - type: string. if present use it as task name instead of taking name from python function
-
-name [required for sub-task]:
-  - type: string. sub-task identifier
-
-file_dep:
-  - type: list. items:
-    * file (string) path relative to the dodo file
-
-task_dep:
-  - type: list. items:
-    * task name (string)
-
-targets:
-  - type: list of strings
-  - each item is file-path relative to the dodo file (accepts both files and folders)
-
-uptodate:
-  - type: list. items:
-    * None - None values are just ignored
-    * bool - False indicates task is not up-to-date
-    * callable - returns bool or None. must take 2 positional parameters (task, values)
-
-calc_dep:
-  - type: list. items:
-    * task name (string)
-
-getargs:
-  - type: dictionary
-    * key: string with the name of the function parater (used in a python-action)
-    * value: string on the format <task-name>.<variable-name>
-
-setup:
- - type: list. items:
-   * task name (string)
-
-teardown:
- - type: (list) of actions (see above)
-
-doc:
- - type: string -> the description text
-
-clean:
- - type: (True bool) remove target files
- - type: (list) of actions (see above)
-
-params:
- - type: (list) of dictionaries containing:
-   - name [required] (string) parameter identifier
-   - default [required] default value for parameter
-   - short [optional] (string - 1 letter) short option string
-   - long [optional] (string) long option string
-   - type [optional] the option will be converted to this type
-
-verbosity:
- - type: int
-   -  0: capture (do not print) stdout/stderr from task.
-   -  1: (default) capture stdout only.
-   -  2: do not capture anything (print everything immediately).
-
-title:
- - type: callable taking one parameter as argument (the task reference)
-"""
 
 
 ##########################################################
@@ -495,21 +436,58 @@ class Help(Command):
     doc_description = None
 
     @staticmethod
-    def execute(params, args):
+    def print_task_help():
+        """print help for 'task' usage """
+        print HELP_TASK
+
+    def execute(self, params, args):
         """execute cmd 'help' """
         if len(args) == 1:
             if args[0] in params['sub']:
                 print params['sub'][args[0]].help()
                 return 0
             elif args[0] == 'task':
-                print_task_help()
+                self.print_task_help()
                 return 0
-        print_usage()
+        DoitMain.print_usage()
         return 0
 
 
 
+
+
+
 DOIT_BUILTIN_CMDS = (Help(), Run(), List(), Clean(), Forget(), Ignore(), Auto())
+
+class DoitMain(object):
+    @staticmethod
+    def print_version():
+        """print doit version (includes path location)"""
+        print ".".join([str(i) for i in doit.__version__])
+        print "bin @", os.path.abspath(__file__)
+        print "lib @", os.path.dirname(os.path.abspath(doit.__file__))
+
+    @staticmethod
+    def print_usage():
+        """print doit "usage" (basic help) instructions"""
+        # TODO cmd list should be automatically generated.
+        print """
+doit -- automation tool
+http://python-doit.sourceforge.net/
+
+Commands:
+ doit [run]             run tasks
+ doit clean             clean action / remove targets
+ doit list              list tasks from dodo file
+ doit forget            clear successful run status from DB
+ doit ignore            ignore task (skip) on subsequent runs
+ doit auto              automatically run doit when a dependency changes
+
+ doit help              show help / reference
+ doit help task         show help on task dictionary fields
+ doit help <command>    show command usage
+"""
+
 def cmd_main(cmd_args):
     """entry point for all commands
 
@@ -522,6 +500,17 @@ def cmd_main(cmd_args):
          So be aware if you expect a different formatting (like JSON)
          from the Reporter.
     """
+
+    # special parameters that dont run anything
+    if cmd_args:
+        if cmd_args[0] == "--version":
+            DoitMain.print_version()
+            return 0
+        if cmd_args[0] == "--help":
+            DoitMain.print_usage()
+            return 0
+
+
     # all sub-commands
     sub_cmd = dict((cmd.name, cmd) for cmd in DOIT_BUILTIN_CMDS)
 
@@ -535,8 +524,17 @@ def cmd_main(cmd_args):
         else:
             args_no_vars.append(arg)
 
+
+    # get specified sub-command or use default='run'
+    if len(args_no_vars) == 0 or args_no_vars[0] not in sub_cmd.keys():
+        command = 'run'
+    else:
+        command = args_no_vars.pop(0)
+
+
+    # execute command
     try:
-        return sub_cmd['run'].parse_execute(args_no_vars, sub=sub_cmd)
+        return sub_cmd[command].parse_execute(args_no_vars, sub=sub_cmd)
 
     # dont show traceback for user errors.
     except (CmdParseError, InvalidDodoFile,
