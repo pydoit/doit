d04180d6ca78c3cb01018b2172079f273cb4ef60
Eduardo Schettino <schettino72@gmail.com>
2012-12-05 11:29:24 +0800

    status 0.19 development

--- doit/__init__.py
+++ doit/__init__.py
@@ -23,7 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
-__version__ = (0, 18, 1)
+__version__ = (0, 19, 'dev0')
 
 
 # used to save variable values passed from command line
