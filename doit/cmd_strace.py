0bac56a9ceb69da5d413e7a8dab138e8db94adc2
Eduardo Schettino <schettino72@gmail.com>
2013-01-05 07:18:34 +0800

    strace - show only files in subfolder b default.

--- doit/cmd_strace.py
+++ doit/cmd_strace.py
@@ -7,6 +7,17 @@ from .task import Task
 from .cmd_run import Run
 
 
+# filter to display only files from cwd
+opt_show_all = {
+    'name':'show_all',
+    'short':'a',
+    'long':'all',
+    'type': bool,
+    'default': False,
+    'help': "display all files (not only from within CWD path",
+    }
+
+
 class Strace(Run):
     doc_purpose = "use strace to list file_deps and targets"
     doc_usage = "TASK"
@@ -21,24 +32,25 @@ This is a debugging feature wiht many lilmitations.
   * if there is more than one action .strace file is overwritten
   * the process being traced itself might have some kind of cache,
     that means it might not write a target file if it exist.
-  * does not handle `chdir`
+  * does not handle chdir
 
 So this is NOT 100% reliable, use with care!
 Error message might not be very clear...
 """
 
-    cmd_options = ()
+    cmd_options = (opt_show_all, )
 
     TRACE_CMD = "strace -f -e trace=file -o %s %s "
     TRACE_OUT = '.strace'
 
     def execute(self, params, args):
+        """remove existing output file if any and do sanity checking"""
         if os.path.exists(self.TRACE_OUT):
             os.unlink(self.TRACE_OUT)
         assert len(args) == 1, 'doit strace failed, must select task to strace'
         return Run.execute(self, params, args)
 
-    def _execute(self):
+    def _execute(self, show_all):
         # find task to trace and wrap it
         selected = self.sel_tasks[0]
         for task in self.task_list:
@@ -49,7 +61,7 @@ Error message might not be very clear...
         # add task to print report
         report_strace = Task(
             'strace_report',
-            actions = [(find_deps, [self.outstream, self.TRACE_OUT])],
+            actions = [(find_deps, [self.outstream, self.TRACE_OUT, show_all])],
             verbosity = 2,
             task_dep = [selected],
             uptodate = [False],
@@ -63,6 +75,7 @@ Error message might not be very clear...
 
     @classmethod
     def wrap_strace(cls, task):
+        """wrap task actions into strace command"""
         wrapped_actions = []
         for action in task.actions:
             cmd = cls.TRACE_CMD % (cls.TRACE_OUT, action._action)
@@ -72,24 +85,41 @@ Error message might not be very clear...
         task._extend_uptodate([False])
 
 
-def find_deps(outstream, strace_out):
+def find_deps(outstream, strace_out, show_all):
     """read file witn strace output, return dict with deps, targets"""
     # 7978  open("/etc/ld.so.cache", O_RDONLY|O_CLOEXEC) = 3
-    # ignore text until '('
-    # get "file" name inside "
-    # comma-space separate other elements ', '
-    # ignore elments if inside []
     # get "mode" file was open, until ')' is closed
     # ignore rest of line
-    regex = re.compile(r'.*\("(?P<file>[^"]*)", (\[.*\])*(?P<mode>[^)]*)\).*')
+    # .*\(                 # ignore text until '('
+    # "(?P<file>[^"]*)"    # get "file" name inside "
+    # , (\[.*\])*          # ignore elments if inside [] - used by execve
+    # (?P<mode>[^)]*)\)    # get mode opening file
+    #  = ].*               # check syscall was successful""",
+    regex = re.compile(r'.*\("(?P<file>[^"]*)", (\[.*\])*(?P<mode>[^)]*)\) = [^-].*')
+
+    read = set()
+    write = set()
+    cwd = os.getcwd()
     with open(strace_out) as text:
         for line in text:
+            # ignore non file operation
             match = regex.match(line)
             if not match:
                 continue
-            name = os.path.abspath(match.group('file'))
+            rel_name = match.group('file')
+            name = os.path.abspath(rel_name)
+
+            # ignore files out of cwd
+            if not show_all:
+                if not name.startswith(cwd):
+                    continue
+
             if 'WR' in match.group('mode'):
-                outstream.write("T %s\n" % name)
+                if name not in write:
+                    write.add(name)
+                    outstream.write("W %s\n" % name)
             else:
-                outstream.write("D %s\n" % name)
+                if name not in read:
+                    read.add(name)
+                    outstream.write("R %s\n" % name)
 
--- tests/test_cmd_strace.py
+++ tests/test_cmd_strace.py
@@ -16,11 +16,25 @@ class TestCmdRun(object):
                     file_dep=['tests/data/dependency1'])
         cmd = Strace(outstream=output)
         cmd._loader.load_tasks = mock.Mock(return_value=([task], {}))
-        result = cmd.execute(DefaultUpdate(dep_file=depfile.name), ['tt'])
+        params = DefaultUpdate(dep_file=depfile.name, show_all=False)
+        result = cmd.execute(params, ['tt'])
         assert 0 == result
-        got = output.getvalue().split("\n")[:-1]
+        got = output.getvalue().split("\n")
         dep_path = os.path.abspath("tests/data/dependency1")
-        assert "D %s" % dep_path in got
+        assert "R %s" % dep_path in got[0]
+
+
+    def test_show_all(self, dependency1, depfile):
+        output = StringIO()
+        task = Task("tt", ["cat %(dependencies)s"],
+                    file_dep=['tests/data/dependency1'])
+        cmd = Strace(outstream=output)
+        cmd._loader.load_tasks = mock.Mock(return_value=([task], {}))
+        params = DefaultUpdate(dep_file=depfile.name, show_all=True)
+        result = cmd.execute(params, ['tt'])
+        assert 0 == result
+        got = output.getvalue().split("\n")
+        assert "cat" in got[0]
 
 
     def test_target(self, dependency1, depfile):
@@ -29,8 +43,9 @@ class TestCmdRun(object):
                     targets=['tests/data/dependency1'])
         cmd = Strace(outstream=output)
         cmd._loader.load_tasks = mock.Mock(return_value=([task], {}))
-        result = cmd.execute(DefaultUpdate(dep_file=depfile.name), ['tt'])
+        params = DefaultUpdate(dep_file=depfile.name, show_all=False)
+        result = cmd.execute(params, ['tt'])
         assert 0 == result
-        got = output.getvalue().split("\n")[:-1]
+        got = output.getvalue().split("\n")
         tgt_path = os.path.abspath("tests/data/dependency1")
-        assert "T %s" % tgt_path in got
+        assert "W %s" % tgt_path in got[0]
