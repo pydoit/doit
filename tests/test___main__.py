52405c4983b38f2536bea63e5402427f5749854a
schettino72 <none@none>
2014-02-15 12:57:29 +1300

    fix test on py2.6. skip python -m test

--- tests/test___main__.py
+++ tests/test___main__.py
@@ -1,5 +1,9 @@
+import sys
 import subprocess
 
+import pytest
+
+@pytest.mark.skipif('sys.version_info < (2,7,0)')
 def test_execute():
     assert 0 == subprocess.call(['python', '-m', 'doit', 'list'])
 
