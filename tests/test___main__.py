deee986b5bda76e6b8b7426a502a86e8308485ad
schettino72 <none@none>
2014-02-18 21:34:27 +1300

    added coverall to travis

--- tests/test___main__.py
+++ tests/test___main__.py
@@ -3,6 +3,7 @@ import subprocess
 
 import pytest
 
+sys # keep pyflakes quiet
 @pytest.mark.skipif('sys.version_info < (2,7,0)')
 def test_execute():
     assert 0 == subprocess.call(['python', '-m', 'doit', 'list'])
