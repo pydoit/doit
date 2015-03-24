1f9c3c755c96508ca2b1b2668f102f9d2da9c614
Eduardo Schettino <schettino72@gmail.com>
2015-03-25 00:58:38 +0800

    fix test on py3.2

--- tests/test_cmd_base.py
+++ tests/test_cmd_base.py
@@ -1,10 +1,10 @@
 import os
 
+import six
 import pytest
 
 from doit import version
 from doit.cmdparse import CmdParseError, CmdOption, CmdParse
-
 from doit.exceptions import InvalidCommand, InvalidDodoFile
 from doit.dependency import FileChangedChecker
 from doit.task import Task
@@ -16,7 +16,7 @@ from doit.cmd_base import check_tasks_exist, tasks_and_deps_iter, subtasks_iter
 def test_version_tuple():
     assert [1,2,3] == version_tuple([1,2,3])
     assert [1,2,3] == version_tuple('1.2.3')
-    assert [1,2,3] == version_tuple(u'1.2.3')
+    assert [1,2,3] == version_tuple(six.u('1.2.3'))
     assert [0,2,0] == version_tuple('0.2.0')
     assert [0,2,-1] == version_tuple('0.2.dev1')
 
