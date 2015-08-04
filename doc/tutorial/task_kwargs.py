5949a004a67772ea7b3af9d9055c3d2aa21fa39c
Frank Stein <dr.frank.stain@gmail.com>
2015-08-04 23:24:48 +0300

    Changing TABs into SPACEs.

--- doc/tutorial/task_kwargs.py
+++ doc/tutorial/task_kwargs.py
@@ -1,13 +1,13 @@
 from __future__ import print_function
 
 def func_with_args(arg_first, arg_second):
-	print(arg_first)
-	print(arg_second)
-	return True
-	
+    print(arg_first)
+    print(arg_second)
+    return True
+    
 def task_call_func():
-	return {
-		'actions': [
-			(func_with_args, [], {'arg_second': 'This is a second argument.', 'arg_first': 'This is a first argument.'})
-		]
-	}
+    return {
+        'actions': [
+            (func_with_args, [], {'arg_second': 'This is a second argument.', 'arg_first': 'This is a first argument.'})
+        ]
+    }
