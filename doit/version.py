9bf60efc8d7a3410caa97903e581c85c47450cf6
Eduardo Schettino <schettino72@gmail.com>
2015-08-16 02:08:51 +0800

    bump version to 0.29.0

--- doit/version.py
+++ doit/version.py
@@ -1,2 +1,2 @@
 """doit version, defined out of __init__.py to avoid circular reference"""
-VERSION = (0, 29, 'dev0')
+VERSION = (0, 29, 0)
--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ configuration management, etc.
 
 setup(name = 'doit',
       description = 'doit - Automation Tool',
-      version = '0.29.dev0',
+      version = '0.29.0',
       license = 'MIT',
       author = 'Eduardo Naufel Schettino',
       author_email = 'schettino72@gmail.com',
