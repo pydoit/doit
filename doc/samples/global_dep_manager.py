ce47f505c6334c1a5827121b286574ad50da8b72
Mike Pagel <mike@mpagel.de>
2019-05-13 07:17:59 +0200

    added example code for global dep_manager

---
+++ doc/samples/global_dep_manager.py
@@ -0,0 +1,32 @@
+import doit.globals
+
+DOIT_CONFIG = dict(
+    verbosity=2,
+)
+
+
+def task_create():
+    # dependency manager is defined for all code inside the generator:
+    dep_manager = doit.globals.Globals.dep_manager
+
+    def action(ident):
+        # assume some involved logic to define ident:
+        ident = 42
+        print('Created', ident)
+
+        # store for clean:
+        return dict(created=ident)
+
+    def clean(task):
+        result = dep_manager.get_result(task.name)
+        if result:
+            ident = result['created']
+            print('Deleted', ident)
+
+            # possibly forget the task, after it was cleaned:
+            dep_manager.remove(task.name)
+
+    return dict(
+        actions=[action],
+        clean=[clean],
+    )
