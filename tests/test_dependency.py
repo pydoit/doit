e03e0d0928a6358f53cf1fdd30d4d1f63c6fa407
Brad Dixon <brad.dixon@carvesystems.com>
2019-06-05 11:28:16 -0400

    WIP: Revert inadvertent code formatting in test directory.
    Sorry about that.

--- tests/test_dependency.py
+++ tests/test_dependency.py
@@ -52,7 +52,6 @@ def test_sqlite_import():
 
     os.remove(filename)
 
-
 ####
 # dependencies are files only (not other tasks).
 #
@@ -66,6 +65,8 @@ def test_sqlite_import():
 # save in db (task - dependency - (timestamp, size, signature))
 # taskId_dependency => signature(dependency)
 # taskId is md5(CmdTask.task)
+
+
 
 # test parametrization, execute tests for all DB backends.
 # create a separate fixture to be used only by this module
@@ -108,7 +109,6 @@ def pdep_manager(request, codecs):
 # FIXME there was major refactor breaking classes from dependency,
 # unit-tests could be more specific to base classes.
 
-
 class TestDependencyDb(object):
     # adding a new value to the DB
     def test_get_set(self, pdep_manager):
@@ -121,14 +121,14 @@ class TestDependencyDb(object):
         value = pdep_manager._get("taskId_我", "dependency_A")
         assert "da_md5" == value, value
 
-    def test_dump(self, pdep_manager, codecs):
+    #
+    def test_dump(self, pdep_manager):
         # save and close db
         pdep_manager._set("taskId_X", "dependency_A", "da_md5")
         pdep_manager.close()
 
         # open it again and check the value
-        encoder_cls, decoder_cls = codecs
-        d2 = Dependency(pdep_manager.db_class, pdep_manager.name, encoder_cls=encoder_cls, decoder_cls=decoder_cls)
+        d2 = Dependency(pdep_manager.db_class, pdep_manager.name)
 
         value = d2._get("taskId_X", "dependency_A")
         assert "da_md5" == value, value
@@ -143,9 +143,8 @@ class TestDependencyDb(object):
             fd = open(full_name, 'w')
             fd.write("""{"x": y}""")
             fd.close()
-        pytest.raises(
-            DatabaseException, Dependency, pdep_manager.db_class, pdep_manager.name
-        )
+        pytest.raises(DatabaseException, Dependency,
+                      pdep_manager.db_class, pdep_manager.name)
 
     def test_corrupted_file_unrecognized_excep(self, monkeypatch, pdep_manager):
         if pdep_manager.db_class is not DbmDB:
@@ -160,18 +159,19 @@ class TestDependencyDb(object):
             fd.write("""{"x": y}""")
             fd.close()
         monkeypatch.setattr(DbmDB, 'DBM_CONTENT_ERROR_MSG', 'xxx')
-        pytest.raises(
-            DatabaseException, Dependency, pdep_manager.db_class, pdep_manager.name
-        )
+        pytest.raises(DatabaseException, Dependency,
+                      pdep_manager.db_class, pdep_manager.name)
 
     # _get must return None if entry doesnt exist.
     def test_getNonExistent(self, pdep_manager):
         assert pdep_manager._get("taskId_X", "dependency_A") == None
 
+
     def test_in(self, pdep_manager):
         pdep_manager._set("taskId_ZZZ", "dep_1", "12")
         assert pdep_manager._in("taskId_ZZZ")
         assert not pdep_manager._in("taskId_hohoho")
+
 
     def test_remove(self, pdep_manager):
         pdep_manager._set("taskId_ZZZ", "dep_1", "12")
@@ -182,21 +182,22 @@ class TestDependencyDb(object):
         assert None == pdep_manager._get("taskId_ZZZ", "dep_2")
         assert "14" == pdep_manager._get("taskId_YYY", "dep_1")
 
+
     # special test for DBM backend and "dirty"/caching mechanism
-    def test_remove_from_non_empty_file(self, pdep_manager, codecs):
-        encoder_cls, decoder_cls = codecs
+    def test_remove_from_non_empty_file(self, pdep_manager):
         # 1 - put 2 tasks of file
         pdep_manager._set("taskId_XXX", "dep_1", "x")
         pdep_manager._set("taskId_YYY", "dep_1", "x")
         pdep_manager.close()
         # 2 - re-open and remove one task
-        reopened = Dependency(pdep_manager.db_class, pdep_manager.name, encoder_cls=encoder_cls, decoder_cls=decoder_cls)
+        reopened = Dependency(pdep_manager.db_class, pdep_manager.name)
         reopened.remove("taskId_YYY")
         reopened.close()
         # 3 - re-open again and check task was really removed
-        reopened2 = Dependency(pdep_manager.db_class, pdep_manager.name, encoder_cls=encoder_cls, decoder_cls=decoder_cls)
+        reopened2 = Dependency(pdep_manager.db_class, pdep_manager.name)
         assert reopened2._in("taskId_XXX")
         assert not reopened2._in("taskId_YYY")
+
 
     def test_remove_all(self, pdep_manager):
         pdep_manager._set("taskId_ZZZ", "dep_1", "12")
@@ -209,6 +210,7 @@ class TestDependencyDb(object):
 
 
 class TestSaveSuccess(object):
+
     def test_save_result(self, pdep_manager):
         t1 = Task('t_name', None)
         t1.result = "result"
@@ -285,39 +287,39 @@ class TestSaveSuccess(object):
 
     def test_save_values(self, pdep_manager):
         t1 = Task('t1', None)
-        t1.values = {'x': 5, 'y': 10}
+        t1.values = {'x':5, 'y':10}
         pdep_manager.save_success(t1)
-        assert {'x': 5, 'y': 10} == pdep_manager._get("t1", "_values_:")
+        assert {'x':5, 'y':10} == pdep_manager._get("t1", "_values_:")
 
 
 class TestGetValue(object):
     def test_all_values(self, pdep_manager):
         t1 = Task('t1', None)
-        t1.values = {'x': 5, 'y': 10}
+        t1.values = {'x':5, 'y':10}
         pdep_manager.save_success(t1)
-        assert {'x': 5, 'y': 10} == pdep_manager.get_values('t1')
+        assert {'x':5, 'y':10} == pdep_manager.get_values('t1')
 
     def test_ok(self, pdep_manager):
         t1 = Task('t1', None)
-        t1.values = {'x': 5, 'y': 10}
+        t1.values = {'x':5, 'y':10}
         pdep_manager.save_success(t1)
         assert 5 == pdep_manager.get_value('t1', 'x')
 
     def test_ok_dot_on_task_name(self, pdep_manager):
         t1 = Task('t1:a.ext', None)
-        t1.values = {'x': 5, 'y': 10}
+        t1.values = {'x':5, 'y':10}
         pdep_manager.save_success(t1)
         assert 5 == pdep_manager.get_value('t1:a.ext', 'x')
 
     def test_invalid_taskid(self, pdep_manager):
         t1 = Task('t1', None)
-        t1.values = {'x': 5, 'y': 10}
+        t1.values = {'x':5, 'y':10}
         pdep_manager.save_success(t1)
         pytest.raises(Exception, pdep_manager.get_value, 'nonono', 'x')
 
     def test_invalid_key(self, pdep_manager):
         t1 = Task('t1', None)
-        t1.values = {'x': 5, 'y': 10}
+        t1.values = {'x':5, 'y':10}
         pdep_manager.save_success(t1)
         pytest.raises(Exception, pdep_manager.get_value, 't1', 'z')
 
@@ -343,7 +345,7 @@ class TestMD5Checker(object):
     def test_timestamp(self, dependency1):
         checker = MD5Checker()
         state = checker.get_state(dependency1, None)
-        state2 = (state[0], state[1] + 1, '')
+        state2 = (state[0], state[1]+1, '')
         file_stat = os.stat(dependency1)
         # dep considered the same as long as timestamp is unchanged
         assert not checker.check_modified(dependency1, file_stat, state2)
@@ -351,7 +353,7 @@ class TestMD5Checker(object):
     def test_size(self, dependency1):
         checker = MD5Checker()
         state = checker.get_state(dependency1, None)
-        state2 = (state[0] + 1, state[1] + 1, state[2])
+        state2 = (state[0]+1, state[1]+1, state[2])
         file_stat = os.stat(dependency1)
         # if size changed for sure modified (md5 is not checked)
         assert checker.check_modified(dependency1, file_stat, state2)
@@ -361,21 +363,23 @@ class TestMD5Checker(object):
         state = checker.get_state(dependency1, None)
         file_stat = os.stat(dependency1)
         # same size and md5
-        state2 = (state[0] + 1, state[1], state[2])
+        state2 = (state[0]+1, state[1], state[2])
         assert not checker.check_modified(dependency1, file_stat, state2)
         # same size, different md5
-        state3 = (state[0] + 1, state[1], 'not me')
+        state3 = (state[0]+1, state[1], 'not me')
         assert checker.check_modified(dependency1, file_stat, state3)
 
 
 class TestCustomChecker(object):
+
     def test_not_implemented(self, dependency1):
         class MyChecker(FileChangedChecker):
             pass
 
         checker = MyChecker()
         pytest.raises(NotImplementedError, checker.get_state, None, None)
-        pytest.raises(NotImplementedError, checker.check_modified, None, None, None)
+        pytest.raises(NotImplementedError, checker.check_modified,
+                      None, None, None)
 
 
 class TestTimestampChecker(object):
@@ -384,7 +388,7 @@ class TestTimestampChecker(object):
         state = checker.get_state(dependency1, None)
         file_stat = os.stat(dependency1)
         assert not checker.check_modified(dependency1, file_stat, state)
-        assert checker.check_modified(dependency1, file_stat, state + 1)
+        assert checker.check_modified(dependency1, file_stat, state+1)
 
 
 class TestDependencyStatus(object):
@@ -423,7 +427,9 @@ class TestDependencyStatus(object):
         assert 'foo xxx' == result.get_error_message()
 
 
+
 class TestGetStatus(object):
+
     def test_ignore(self, pdep_manager):
         t1 = Task("t1", None)
         # before ignore
@@ -431,6 +437,7 @@ class TestGetStatus(object):
         # after ignote
         pdep_manager.ignore(t1)
         assert pdep_manager.status_is_ignore(t1)
+
 
     def test_fileDependencies(self, pdep_manager):
         filePath = get_abspath("data/dependency1")
@@ -490,6 +497,7 @@ class TestGetStatus(object):
         assert 'run' == pdep_manager.get_status(t1, {}).status
         assert [] == t1.dep_changed
 
+
     def test_fileDependencies_changed_get_log(self, pdep_manager):
         filePath = get_abspath("data/dependency1")
         ff = open(filePath, "w")
@@ -517,10 +525,12 @@ class TestGetStatus(object):
         assert [filePath] == result.reasons['removed_file_dep']
         assert [filePath2] == result.reasons['added_file_dep']
 
+
     def test_file_dependency_not_exist(self, pdep_manager):
         filePath = get_abspath("data/dependency_not_exist")
         t1 = Task("t1", None, [filePath])
         assert 'error' == pdep_manager.get_status(t1, {}).status
+
 
     def test_change_checker(self, pdep_manager, dependency1):
         t1 = Task("taskId_X", None, [dependency1])
@@ -533,6 +543,7 @@ class TestGetStatus(object):
         pdep_manager.save_success(t1)
         assert 'up-to-date' == pdep_manager.get_status(t1, {}).status
 
+
     # if there is no dependency the task is always executed
     def test_noDependency(self, pdep_manager):
         t1 = Task("t1", None)
@@ -543,6 +554,7 @@ class TestGetStatus(object):
         pdep_manager.save_success(t1)
         assert 'run' == pdep_manager.get_status(t1, {}).status
         assert [] == t1.dep_changed
+
 
     def test_UptodateFalse(self, pdep_manager):
         filePath = get_abspath("data/dependency1")
@@ -582,11 +594,11 @@ class TestGetStatus(object):
         pdep_manager.save_success(t1)
         assert 'up-to-date' == pdep_manager.get_status(t1, {}).status
 
+
     def test_UptodateFunction_True(self, pdep_manager):
         def check(task, values):
             assert task.name == 't1'
             return True
-
         t1 = Task("t1", None, uptodate=[check])
         pdep_manager.save_success(t1)
         assert 'up-to-date' == pdep_manager.get_status(t1, {}).status
@@ -619,14 +631,13 @@ class TestGetStatus(object):
 
     def test_uptodate_call_all_even_if_some_False(self, pdep_manager):
         checks = []
-
         def check():
             checks.append(1)
             return False
-
         t1 = Task("t1", None, uptodate=[check, check])
         assert 'run' == pdep_manager.get_status(t1, {}).status
         assert 2 == len(checks)
+
 
     def test_UptodateFunction_extra_args_True(self, pdep_manager):
         def check(task, values, control):
@@ -656,7 +667,6 @@ class TestGetStatus(object):
 
     def test_UptodateCallable_added_attributes(self, pdep_manager):
         task_dict = "fake dict"
-
         class My_uptodate(UptodateCalculator):
             def __call__(self, task, values):
                 # attributes were added to object before call'ing it
@@ -678,6 +688,7 @@ class TestGetStatus(object):
         pdep_manager.save_success(t1)
         assert 'run' == pdep_manager.get_status(t1, {}).status
 
+
     # if target file does not exist, task is outdated.
     def test_targets_notThere(self, pdep_manager, dependency1):
         target = get_abspath("data/target")
@@ -688,6 +699,7 @@ class TestGetStatus(object):
         pdep_manager.save_success(t1)
         assert 'run' == pdep_manager.get_status(t1, {}).status
         assert [dependency1] == t1.dep_changed
+
 
     def test_targets(self, pdep_manager, dependency1):
         filePath = get_abspath("data/target")
@@ -703,6 +715,7 @@ class TestGetStatus(object):
         # up-to-date because target exist
         assert 'up-to-date' == pdep_manager.get_status(t1, {}).status
         assert [] == t1.dep_changed
+
 
     def test_targetFolder(self, pdep_manager, dependency1):
         # folder not there. task is not up-to-date
