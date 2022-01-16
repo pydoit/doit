ecde6bd1c61d65bb69510069bab8ce589e5eb510
Eduardo Schettino <schettino72@gmail.com>
2022-01-16 13:05:23 +0800

    docs: remove old homepage from built html.

--- doc/conf.py
+++ doc/conf.py
@@ -69,7 +69,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'presentation.rst']
+exclude_patterns = ['_build', 'presentation.rst', 'old']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
