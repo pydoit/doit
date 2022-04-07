1d4dfff48c1b35e19f6455cbe46ed36c75c9f20b
Eduardo Schettino <schettino72@gmail.com>
2022-04-07 21:54:15 +0800

    prepare release 0.35.0

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ author = 'Eduardo Schettino (schettino72)'
 # The short X.Y version
 version = '0.35'
 # The full version, including alpha/beta/rc tags
-release = '0.35.0dev'
+release = '0.35.0'
 
 
 # -- General configuration ---------------------------------------------------
