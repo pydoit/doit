a20e01f6ebc0c7896c0c777dec212265b431a346
Eduardo Schettino <schettino72@gmail.com>
2022-04-07 14:21:41 +0800

    add `api.py::run_tasks()`.

--- doit/api.py
+++ doit/api.py
@@ -2,8 +2,11 @@
 
 import sys
 
-from doit.cmd_base import ModuleTaskLoader
+from doit.cmdparse import CmdParseError
+from doit.exceptions import InvalidDodoFile, InvalidCommand, InvalidTask
+from doit.cmd_base import ModuleTaskLoader, get_loader
 from doit.doit_cmd import DoitMain
+
 
 def run(task_creators):
     """run doit using task_creators
@@ -11,3 +14,38 @@ def run(task_creators):
     @param task_creators: module or dict containing task creators
     """
     sys.exit(DoitMain(ModuleTaskLoader(task_creators)).run(sys.argv[1:]))
+
+
+
+
+def run_tasks(loader, tasks, extra_config=None):
+    """run DoitMain instance with speficied tasks and parameters
+
+    :params tasks: list of task names (str)
+    """
+    loader.task_opts = tasks # task_opts will be used as @task_param
+    main = DoitMain(loader, extra_config=extra_config)
+    task_names = list(tasks.keys())
+
+    # get list of available commands
+    sub_cmds = main.get_cmds()
+    task_loader = get_loader(main.config, main.task_loader, sub_cmds)
+
+    # execute command
+    cmd_name = 'run'
+    command = sub_cmds.get_plugin(cmd_name)(
+        task_loader=task_loader,
+        config=main.config,
+        bin_name=main.BIN_NAME,
+        cmds=sub_cmds,
+        opt_vals={},
+        )
+
+    try:
+        return command.parse_execute(task_names)
+    except (CmdParseError, InvalidDodoFile,
+            InvalidCommand, InvalidTask) as err:
+        if isinstance(err, InvalidCommand):
+            err.cmd_used = cmd_name
+            err.bin_name = main.BIN_NAME
+        raise err
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,10 @@
 import sys
 
-from doit.api import run
+from doit.cmd_base import ModuleTaskLoader
+from doit.api import run, run_tasks
 
 
-def test_execute(monkeypatch, depfile_name):
+def test_run(monkeypatch, depfile_name):
     monkeypatch.setattr(sys, 'argv', ['did', '--db-file', depfile_name])
     try:
         def hi():
@@ -15,3 +16,57 @@ def test_execute(monkeypatch, depfile_name):
         assert err.code == 0
     else: # pragma: no cover
         assert False
+
+
+
+def _dodo():
+    """sample tasks"""
+    def hi(opt=None):
+        print('hi', opt)
+
+    def task_hi():
+        return {
+            'actions': [hi],
+            'params': [{'name': 'opt', 'default': '1'}],
+        }
+    def task_two():
+        def my_error():
+            return False
+        return {
+            'actions': [my_error],
+        }
+
+    return {
+        'task_hi': task_hi,
+        'task_two': task_two,
+    }
+
+
+def test_run_tasks_success(capsys, depfile_name):
+    result = run_tasks(
+        ModuleTaskLoader(_dodo()),
+        {'hi': {'opt': '3'}},
+        extra_config = {
+            'GLOBAL': {
+                'verbosity': 2,
+                'dep_file': depfile_name,
+            },
+        },
+    )
+    assert result == 0
+    out = capsys.readouterr().out
+    assert out.strip() == 'hi 3'
+
+
+def test_run_tasks_error(capsys, depfile_name):
+    result = run_tasks(
+        ModuleTaskLoader(_dodo()),
+        {'two': None},
+        extra_config = {
+            'GLOBAL': {
+                'verbosity': 2,
+                'dep_file': depfile_name,
+            },
+        },
+    )
+    assert result == 1
