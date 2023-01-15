d829918addb0e8a4fbae2fc8c55b9d7721b8d065
Eduardo Schettino <schettino72@gmail.com>
2023-01-15 13:48:29 -0300

    CI: workflow fixes.

--- tests/test_cmd_dumpdb.py
+++ tests/test_cmd_dumpdb.py
@@ -1,3 +1,5 @@
+from dbm import whichdb
+
 import pytest
 
 from doit.cmd_dumpdb import DumpDB
@@ -5,11 +7,12 @@ from doit.cmd_dumpdb import DumpDB
 class TestCmdDumpDB(object):
 
     def testDefault(self, capsys, dep_manager):
-        if dep_manager.whichdb in ('dbm', 'dbm.ndbm'): # pragma: no cover
-            pytest.skip('%s not supported for this operation' % dep_manager.whichdb)
         # cmd_main(["help", "task"])
         dep_manager._set('tid', 'my_dep', 'xxx')
         dep_manager.close()
+        dbm_kind = whichdb(dep_manager.name)
+        if dbm_kind in ('dbm', 'dbm.ndbm'): # pragma: no cover
+            pytest.skip(f'"{dbm_kind}" not supported for this operation')
         cmd_dump = DumpDB()
         cmd_dump.execute({'dep_file': dep_manager.name}, [])
         out, err = capsys.readouterr()
