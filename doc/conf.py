447d7c20bed381054fe29d4ee2d48dcaa8b94a81
Eduardo Schettino <schettino72@gmail.com>
2026-02-09 01:13:14 +0800

    site: add sidebar links + fixes.

--- doc/conf.py
+++ doc/conf.py
@@ -76,6 +76,7 @@ pygments_style = None
 
 # non-default configuration for doit
 html_favicon = '_static/favico.ico'
+html_title = 'pydoit'
 html_show_sourcelink = False
 html_extra_path = ['index.html', 'robots.txt', 'google726fc03ab55ebbfc.html']
 html_logo = '_static/doit-logo-small.png'
@@ -106,13 +107,35 @@ html_static_path = ['_static']
 #
 # The default sidebars (for documents that don't match any pattern) are
 # defined by theme itself.  Builtin themes are using these templates by
-# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
-# 'searchbox.html']``.
-#
+html_sidebars = {
+    '**': [
+        'sidebar/brand.html',
+        'sidebar/links.html',
+        'sidebar/search.html',
+        'sidebar/scroll-start.html',
+        'sidebar/navigation.html',
+        'sidebar/scroll-end.html',
+    ]
+}
+
 html_theme_options = {
     "source_repository": "https://github.com/pydoit/doit/",
     "source_branch": "master",
     "source_directory": "doc/",
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/pydoit/doit",
+            "html": '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>',
+            "class": "",
+        },
+        {
+            "name": "X/Twitter",
+            "url": "https://x.com/pydoit",
+            "html": '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16"><path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/></svg>',
+            "class": "",
+        },
+    ],
 }
 
 
