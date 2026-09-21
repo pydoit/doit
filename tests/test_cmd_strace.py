0d8135a20e754db5472c8852d95f0f7cb720a948
Eduardo Schettino <schettino72@gmail.com>
2026-09-20 23:56:15 -0300

    ci: test fixes mac/win.

--- tests/test_cmd_strace.py
+++ tests/test_cmd_strace.py
@@ -1,6 +1,7 @@
 import os
 import os.path
 import shutil
+import sys
 import unittest
 from io import StringIO
 
@@ -13,7 +14,10 @@ from doit.cmd_strace import Strace
 from tests.support import CmdFactory, DepfileNameMixin, DependencyFileMixin
 
 
-@unittest.skipUnless(shutil.which('strace'), 'strace not available')
+# windows/cygwin ship an MSYS strace that does not support `-e trace=file`,
+# so finding it on PATH is not enough
+@unittest.skipUnless(sys.platform not in ('win32', 'cygwin') and shutil.which('strace'),
+                     'strace not available')
 class TestCmdStrace(DependencyFileMixin, DepfileNameMixin, unittest.TestCase):
 
     @staticmethod
